06886b6f8b052459ad7012718d7c1a568185f62a
Brady Gin <bradygin3@gmail.com>
2023-10-15 20:37:40 -0700

    Fixed compile issues and organization issues
    Added Makefile to do this and this should resolve the issue
    so now the files are organized correctly
    
    I will write a desc in the readMe on how to run the program now with
    these changes.

---
+++ src/lex.cpp
@@ -0,0 +1,37 @@
+#include "lexer.h"
+#include "token.h"
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+int main() {
+    std::cout << "Enter your input:" << std::endl;
+
+    // Initialize the lexer with standard input (cin)
+    Lexer lexer(std::cin);
+
+    try {
+        /* Create a vector of tokens by calling the lexer's tokenize() member function which uses 
+        the nextToken() helper function to create tokens and then pushes them into a vector */
+        //std::cout << "Before tokenize()" << std::endl << std::flush;
+
+        std::vector<Token> tokens = lexer.tokenize();
+
+        //std::cout << "After tokenize()" << std::endl << std::flush;
+
+
+        // Print the tokens and their line and column numbers
+      for (const Token& token : tokens) {
+            std::cout << std::setw(4) << token.line << std::setw(4) << token.column << "  "
+                      << std::setw(4) << token.text << std::endl;
+        }     
+        //std::cout << "After print" << std::endl << std::flush;
+
+    } catch (const std::runtime_error& error) {
+        // Handle syntax errors
+        std::cerr << error.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
