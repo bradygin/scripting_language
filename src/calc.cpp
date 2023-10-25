d46d830f83ab187163b14e7455e5cae06fa25632
Towela Phiri <welaphiri@gmail.com>
2023-10-25 10:50:39 -0700

    debugging memory issues

--- src/calc.cpp
+++ src/calc.cpp
@@ -7,41 +7,31 @@
 #include "lib/infixParser.h" 
 
 int main() {
-    while (true) {
-        // Read one line of input
-        std::string inputLine;
-        if (!std::getline(std::cin, inputLine)) {
-            // End of input
-            break;
-        }
+    // Initialize the lexer with standard input (cin)
+    Lexer lexer(std::cin);
 
-        // Initialize the lexer with the current line
-        std::istringstream inputStream(inputLine);
-        Lexer lexer(inputStream);
+    try {
+        std::vector<Token> tokens = lexer.tokenize();
+   
+    Parser parser(tokens);
+    ASTNode* root = parser.parse();
 
-        try {
-            // Tokenize and parse the current line
-            std::vector<Token> tokens = lexer.tokenize();
-            Parser parser(tokens);
-            ASTNode* root = parser.parse();
+    if (root) {
+        // Print the AST in infix notation
+        std::string infixExpression = parser.printInfix(root);
+        std::cout << infixExpression << std::endl;
 
-            if (root) {
-                // Print the AST in infix notation
-                std::string infixExpression = parser.printInfix(root);
-                std::cout << infixExpression << std::endl;
+        // Evaluate the expression
+        double result = root->evaluate();
+        std::cout << result << std::endl;
+    } else {
+        std::cerr << "Failed to parse the input expression." << std::endl;
+    }
 
-                // Evaluate the expression
-                double result = root->evaluate();
-                std::cout << result << std::endl;
-                //delete root;
-            } else {
-                std::cerr << "Failed to parse the input expression." << std::endl;
-            }
-        delete root;
-        } catch (const std::runtime_error& error) {
-            // Handle syntax errors
-            std::cerr << error.what() << std::endl;
-        }
+    } catch (const std::runtime_error& error) {
+        // Handle syntax errors
+        std::cerr << error.what() << std::endl;
+        return 1;
     }
 
     return 0;
