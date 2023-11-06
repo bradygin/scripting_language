077e84bde001a1b39937f71e88fef580d6d279d8
Towela Phiri <welaphiri@gmail.com>
2023-11-06 14:26:41 -0800

    error handling

--- src/lib/infixParser.cpp
+++ src/lib/infixParser.cpp
@@ -21,8 +21,7 @@ double Variable::evaluate(std::map<std::string, double>& symbolTable) const {
     if (symbolTable.find(variableName) != symbolTable.end()) {
             return symbolTable.at(variableName);
         } else {
-            //throw UnknownIdentifierException(symbolTable, variableName);
-            throw UnexpectedTokenException(variableName, 1, 1);
+            throw UnknownIdentifierException(symbolTable, variableName);
         }
 }
 
