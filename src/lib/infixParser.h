919e328c3ca6288ce532e6a61bb27486495d0064
Towela Phiri <welaphiri@gmail.com>
2023-11-07 22:48:16 -0800

    error

--- src/lib/infixParser.h
+++ src/lib/infixParser.h
@@ -221,7 +221,7 @@ public:
 
 class InvalidOperandTypeException : public std::runtime_error {
 public:
-    InvalidOperandTypeException() : std::runtime_error("Runtime error: invalid operand type.") {}
+    InvalidOperandTypeException() : std::runtime_error("Runtime error: condition is not a bool.") {}
 
     int getErrorCode() const {
         return 3; 
