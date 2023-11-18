ccee8d294913701481a50be8356310e7e00db3a7
daisyzhongg <124753244+daisyzhongg@users.noreply.github.com>
2023-11-18 13:37:35 -0800

    fix leak

--- src/format.cpp
+++ src/format.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -10,17 +8,15 @@
 #include "lib/infixParser.h"
 
 int main() {
-    std::map<std::string, double> symbolTable; // Create the symbol table
     Lexer lexer(std::cin);
     
     try {
         std::vector<Token> tokens = lexer.tokenize();
-        infixParser parser(tokens, symbolTable);
+        infixParser parser(tokens);
         while (auto root = parser.infixparse()) {              
             // Print the AST in infix notation
             std::string infixExpression = parser.printInfix(root);
             std::cout << infixExpression << std::endl;
-            delete root;
         }
     } catch (const SyntaxError& error) {
         std::cout << error.what() << std::endl;
